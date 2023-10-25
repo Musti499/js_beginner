d7fab1931ca327a302a7fcbd91753b2ad54c42fb
Musti499 <Mustafa1231021@gmail.com>
2023-10-25 20:05:52 +0100

    I learnt data types and variables

---
+++ helloo.js
@@ -0,0 +1,14 @@
+ var salary; //  
+ console.log(salary * 900)
+
+ console.log( salary * 999)
+
+ var tax  = "mustafa"
+ console.log(tax)
+ console.log (tax * salary)
+ var firstname = "Mustafa" // string
+ var lastname = "abdi" // string 
+ var age = 20 // number 
+ 
+ var ismarried = ["mariam", "said" , "bilal"]
+ console.log("friends")
